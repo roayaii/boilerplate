2dc851da2acbae5a85c479aee6f93534686aa07e
roayaii <mohsenroayaei78@gmail.com>
2024-04-24 12:36:58 +0330

    init test

--- pages/blog/[id]/index.vue
+++ pages/blog/[id]/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-// import { seRoute } from 'vue-router';
+// import { useRoute } from 'vue-router';
 
 const route = useRoute()
 </script>
 
 <template >
     <div>
-        <h1>
+        <h1 class="p-4 bg-red-700 " >
             Blog Single Page
         </h1>
         <p>
