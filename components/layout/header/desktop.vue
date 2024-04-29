5048ef86bf231d82e839086be6b2bac3ac70c5d5
roayaii <mohsenroayaei78@gmail.com>
2024-04-29 22:58:09 +0330

    Create layout/header/desktop

---
+++ components/layout/header/desktop.vue
@@ -0,0 +1,13 @@
+<template >
+    <div class="bg-slate-200 py-3">
+        <div class="container">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center gap-3">
+                    <nuxt-link class="p-2 leading-3 text-teal-700">Home</nuxt-link>
+                    <nuxt-link class="p-2 leading-3 text-teal-700">About</nuxt-link>
+                    <nuxt-link class="p-2 leading-3 text-teal-700">Blog</nuxt-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
