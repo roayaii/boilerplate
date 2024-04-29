0987d2d3f2d3144e68032216ab967b360221c850
roayaii <mohsenroayaei78@gmail.com>
2024-04-29 22:58:24 +0330

    Create layout/header/index

---
+++ components/layout/header/index.vue
@@ -0,0 +1,22 @@
+<script setup >
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const greaterThanLg = breakpoints.greaterOrEqual("lg")
+const lg = ref(true)
+
+
+watch(
+    greaterThanLg ,
+    () => {
+        lg.value =  greaterThanLg.value
+    },
+    {immediate: true}
+)
+
+</script>
+
+<template >
+
+    <LayoutHeaderDesktop v-if="lg" />
+    <LayoutHeaderMobile v-else />
+</template>
