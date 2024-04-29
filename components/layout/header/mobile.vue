3109450830ce6934446de0ae4af4f5d2c8a799bf
roayaii <mohsenroayaei78@gmail.com>
2024-04-29 22:58:35 +0330

    Create layout/header/mobile

---
+++ components/layout/header/mobile.vue
@@ -0,0 +1,24 @@
+<template >
+    <div class="bg-slate-200 py-2">
+        <div class="px-4 md:container flex items-center justify-between">
+            <div class="flex items-center gap-3 text-teal-900">
+                <nuxt-link to="/" class="font-medium text-lg" >
+                    Home
+                </nuxt-link>
+                <nuxt-link to="/blog" class="" >
+                    Blog
+                </nuxt-link>
+            </div>
+            <div>
+                <Icon class="w-8 h-8" name="uil:github" color="#134E4A" />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script setup >
+const router = useRouter()
+
+</script>
