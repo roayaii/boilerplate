f46a9c656ef10eb4970eec0abb45a288d0adcf11
roayaii <mohsenroayaei78@gmail.com>
2024-05-05 20:15:34 +0330

    feat : start using dark mode

--- components/layout/header/mobile.vue
+++ components/layout/header/mobile.vue
@@ -1,5 +1,5 @@
 <template >
-    <div class="bg-slate-200 py-2">
+    <div class="bg-slate-200 dark:bg-slate-800 py-2">
         <div class="px-4 md:container flex items-center justify-between">
             <div class="flex items-center gap-3 text-teal-900">
                 <nuxt-link to="/" class="font-medium text-lg" >
