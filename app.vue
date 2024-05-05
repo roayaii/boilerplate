524d1e42ce27cea209bd69b2ae21d50955a5c57f
roayaii <mohsenroayaei78@gmail.com>
2024-05-05 20:15:04 +0330

    fix html head

--- app.vue
+++ app.vue
@@ -6,3 +6,23 @@
     </NuxtLayout>
   </div>
 </template>
+
+
+<script setup >
+const {theme} = useTheme()
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My boilerPlate' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  htmlAttrs: {
+    class: 'dark'
+  }
+
+})
+
+
+</script>
