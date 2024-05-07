165fd15bdc0c749a3f4c833bb1099494efa6454c
roayaii <mohsenroayaei78@gmail.com>
2024-05-07 11:01:13 +0330

    start using dark mode

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -1,9 +1,35 @@
-import { useLocalStorage } from "@vueuse/core"
+import { useLocalStorage, useDark, useToggle } from "@vueuse/core"
+
 export default function () {
-    const theme = useLocalStorage('userPrefferedTheme', () => 'normal')
+    const isDark = useDark({
+        selector: 'html',
+        attribute: 'class',
+        valueDark: 'dark',
+        valueLight: 'light',
+    })
+    
+    // const theme = useLocalStorage('userPrefferedTheme', () => false)
+
+
+    function setDark() {
+        console.log('dark theme')
+        console.log(isDark.value)
+        isDark.value = true
+
+    }
+
+
+
+    function setLight () {
+        console.log('light theme')
+        console.log(isDark.value)
+        isDark.value = false
+    }
 
     
     return {
-        theme,
+        isDark ,
+        setDark,
+        setLight,
     }
 }
